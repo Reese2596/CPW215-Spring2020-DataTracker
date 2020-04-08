55c5dbbfb34f712fedc40abf0926ebe0c27678f3
Makayla Reese <makaylareese15@gmail.com>
2020-04-08 15:30:06 -0700

    Add project files.

---
+++ DataTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DataTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
