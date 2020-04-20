9c76925b1ac9bcd96ffecedcfd4e3ba3cf18b4db
Makayla Reese <makaylareese15@gmail.com>
2020-04-20 10:05:30 -0700

    Added individual authorization to project

--- DataTracker/Datas/ApplicationDbContext.cs
+++ DataTracker/Datas/ApplicationDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
